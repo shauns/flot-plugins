afa04ae680503078178aacdc070efa1667370c8c
shauns <shaun_stanworth@wordbank.com>
2012-02-24 10:10:20 +0000

    Attempting to draw a rectangle for the actual value

--- jquery.flot.bullet.js
+++ jquery.flot.bullet.js
@@ -54,7 +54,7 @@ ranges.
 				plot.getPlaceholder().append('<div class="actual"'
 				 	+ 'style="position:absolute;left:' + (actualOffset.left + 4) + 'px;top:'
 				 	+ (actualOffset.top - (height - 4) * 2) + 'px;">'+bulletOpts.actual+'</div>');
-				ctx.fillRect(actualOffset.left - 1, actualOffset.top - (height / 1.5), 2, height * 1.5);
+				ctx.fillRect(0, actualOffset.top - (height / 1.5), actualOffset.left, height * 1.5);
 				ctx.save();
 			}
 		}
