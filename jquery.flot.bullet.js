3db39f8e236ff8145e888333114553aad59b4cd0
shauns <shaun_stanworth@wordbank.com>
2012-02-24 10:27:00 +0000

    Use the correct width for the actual bar

--- jquery.flot.bullet.js
+++ jquery.flot.bullet.js
@@ -56,7 +56,7 @@ ranges.
 				plot.getPlaceholder().append('<div class="actual"'
 				 	+ 'style="position:absolute;left:' + (actualOffset.left + 4) + 'px;top:'
 				 	+ (actualOffset.top - (height - 4) * 2) + 'px;">'+bulletOpts.actual+'</div>');
-				ctx.fillRect(zeroOffset.left, actualOffset.top - (height / 1.5), actualOffset.left, height * 0.5);
+				ctx.fillRect(zeroOffset.left, actualOffset.top - (height / 1.5), actualOffset.left - zeroOffset.left, height * 0.5);
 				ctx.save();
 			}
 		}
